abef28306e2e4911484a66dfadd26cf7ea14eabb
Sravyareddysoodin <sravyareddy.soodini@cuw.edu>
2025-09-11 00:08:06 +0000

    Week 1: Added revision.cpp with optimized class Solution implementation

--- (1) Project Indices/src/revision.cpp
+++ (1) Project Indices/src/revision.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <unordered_map>
+#include <iostream>
 using namespace std;
 
 class Solution {
@@ -16,3 +17,26 @@ public:
         return {}; // Return empty vector if no solution (though problem guarantees one solution)
     }
 };
+
+int main() {
+    Solution solution;
+    // Test Case 1: nums = [2,7,11,15], target = 9
+    vector<int> nums1 = {2, 7, 11, 15};
+    int target1 = 9;
+    vector<int> result1 = solution.twoSum(nums1, target1);
+    cout << "Test 1: [" << result1[0] << "," << result1[1] << "]" << endl;
+
+    // Test Case 2: nums = [3,2,4], target = 6
+    vector<int> nums2 = {3, 2, 4};
+    int target2 = 6;
+    vector<int> result2 = solution.twoSum(nums2, target2);
+    cout << "Test 2: [" << result2[0] << "," << result2[1] << "]" << endl;
+
+    // Test Case 3: nums = [3,3], target = 6
+    vector<int> nums3 = {3, 3};
+    int target3 = 6;
+    vector<int> result3 = solution.twoSum(nums3, target3);
+    cout << "Test 3: [" << result3[0] << "," << result3[1] << "]" << endl;
+
+    return 0;
+}
